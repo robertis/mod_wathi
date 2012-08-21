0bbc4a044cc2868d6dc609261d00f43f81d99a67
Robertis <rtongbram@gmail.com>
2012-08-20 22:49:25 -0700

    adding the module file

---
+++ apache_module/mod_wathi.c
@@ -0,0 +1,48 @@
+
+#include <httpd.h>
+#include <http_protocol.h>
+#include <http_config.h>
+#include <stdbool.h>
+
+#include "../sdk/wathi_sdk.h"
+
+
+static int my_log_hook(request_rec *r)
+{
+  double response_time; 
+  apr_time_t start_time= r->request_time;
+  apr_time_t current_time= apr_time_now();
+  apr_time_t time_diff = current_time - start_time;
+  bool is_millis = false;
+  if(time_diff < 1000000){
+      //log time in milliseconds
+      is_millis = true;
+      response_time = (double)time_diff/1000;
+  }
+  else{
+      //log time in seconds
+      response_time = (double)time_diff/1000000;
+  }
+  log_request(r->method, r->unparsed_uri, response_time, is_millis);
+
+}
+
+
+static void mycheck_hooks(apr_pool_t *pool)
+{
+  ap_hook_log_transaction(my_log_hook, NULL, NULL, APR_HOOK_MIDDLE);
+}
+
+module AP_MODULE_DECLARE_DATA mycheck_module = {
+    STANDARD20_MODULE_STUFF,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    mycheck_hooks
+} ;
+
+
+
+
